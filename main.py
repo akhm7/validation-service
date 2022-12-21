a3da23552151e79bc2dc7efdae323d44df2afee6
Khojiakbar <57988736+akhm7@users.noreply.github.com>
2022-12-21 20:19:19 +0500

    added db logging, aml checking, updated endpoint contracts

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 from datetime import datetime
 from http.client import HTTPException
+import json
+import string
 
-from fastapi import FastAPI, Request, status
+from fastapi import FastAPI, Request, status, APIRouter, Depends
 from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError, RequestValidationError
 from fastapi.responses import JSONResponse
 import fastapi.openapi.utils as fu
 from pydantic import BaseModel
-import csv, re, logging
+import csv, re, logging, sys, time
+import aiomysql, asyncio
 
+loop = asyncio.get_event_loop()
 logging.basicConfig(filename='/code/logs/{:%Y-%m-%d}.log'.format(datetime.now()), filemode='w', format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S')
 app = FastAPI()
+api_router = APIRouter()
+
+# @app.middleware("http")
+# async def log_requests(request: Request, call_next):
+#     response = await call_next(request)
+#     request_body = await request.json()
+
+#     logging.info(
+#         f"{request.method} request to {request.url} metadata\n"
+#         f"\tHeaders: {request.headers}\n"
+#         f"\tBody: {request_body}\n"
+#         f"\tPath Params: {request.path_params}\n"
+#         f"\tQuery Params: {request.query_params}\n"
+#         f"\tCookies: {request.cookies}\n"
+#     )
+    
+#     return response
+
 
 # and override the schema
 fu.validation_error_response_definition = {
@@ -56,35 +78,52 @@ def lunh(cardNo):
     else:
         return False ## Card Failed Check
 
+async def aml(name):
+    conn = await aiomysql.connect(host="195.158.1.135", port=3306, user="root", password="V1ZkNGRtTlhSbWxaVnpWeQ==", db="validation-service")
+    cur = await conn.cursor()
+
+    sql = "SELECT * FROM blacklist WHERE name = \"%s\"" % (name)
+    print(name)
+    
+    await cur.execute(sql)
+    r = await cur.fetchall()
+    return bool(r)
+
+async def banlist(pan):
+    conn = await aiomysql.connect(host="195.158.1.135", port=3306, user="root", password="V1ZkNGRtTlhSbWxaVnpWeQ==", db="validation-service")
+    cur = await conn.cursor()
+
+    sql = "SELECT * FROM blacklist_pan WHERE pan = %d" % (pan)
+    
+    await cur.execute(sql)
+    r = await cur.fetchall()
+    return bool(r)
+
+async def whitelist(pan):
+    conn = await aiomysql.connect(host="195.158.1.135", port=3306, user="root", password="V1ZkNGRtTlhSbWxaVnpWeQ==", db="validation-service")
+    cur = await conn.cursor()
+
+    sql = "SELECT * FROM whitelist WHERE pan = %d" % (pan)
+    
+    await cur.execute(sql)
+    r = await cur.fetchall()
+    return bool(r)
+
 class Card(BaseModel):
     iin: int | None = None
     recipient: str | None = None
     system: str | None = None
     #currency: str | None = None
-
 class Lookup(BaseModel):
     verify: bool | None = None
     card: Card | None = None
-
 class Payment(BaseModel):
     pan: int
-    expire: int
-    pan2: int
-    amount: int
-    ccy_code: int
-    merchant_id: str | None = None
-    terminal_id: str | None = None
     recipient_name: str
-    tax_id_no: str | None = None
-    client_name: str
-    client_address: str
-    client_city: str
-    get_available_amount: int | None = None
-
 class Validation(BaseModel):
     confirm: bool
     signature: bool
-    msg: str | None = None
+    code: int
 
 @app.get("/")
 async def root():
@@ -92,6 +131,13 @@ async def root():
 
 @app.get("/lookup/{card}", response_model=Lookup)
 async def lookup(card):
+    sql = "INSERT INTO requests (datetime, request, endpoint) VALUES (NOW(), \"%s\", \"%s\")" % (card, "/lookup")
+    print(sql)
+    conn = await aiomysql.connect(host="195.158.1.135", port=3306, user="root", password="V1ZkNGRtTlhSbWxaVnpWeQ==", db="validation-service")
+    cur = await conn.cursor()
+    await cur.execute(sql)
+    await conn.commit()
+    
     if not card:
         raise HTTPException(status_code=404, detail="Card field is required")
     res = {"verify": None, "card": None}
@@ -110,8 +156,32 @@ async def lookup(card):
 
 @app.post("/confirm", response_model=Validation)
 async def confirm(payment: Payment):
+    sql = "INSERT INTO requests (datetime, request, endpoint) VALUES (NOW(), %s, \"%s\")" % (json.dumps(payment.json()), "/confirm")
+    print(sql)
+    conn = await aiomysql.connect(host="195.158.1.135", port=3306, user="root", password="V1ZkNGRtTlhSbWxaVnpWeQ==", db="validation-service")
+    cur = await conn.cursor()
+    await cur.execute(sql)
+    await conn.commit()
+    _confirm = True
+    _code = 0
+
+    aml_check = await aml(payment.recipient_name)
+    print(aml_check)
+    pan_check = await banlist(payment.pan)
+    print(pan_check)
+    ignore = await whitelist(payment.pan)
+    print(ignore)
+    _confirm = ignore or (not pan_check and not aml_check)
+
+    if (pan_check):
+        _code = 12
+    if (aml_check):
+        _code = 11
+    if (aml_check and pan_check):
+        _code = 10
+
     logging.warning('%s', payment)
     if not payment:
-        raise HTTPException(status_code=404, detail="Payment fields not found")
-    panCheck = lunh(str(payment.pan2))
-    return {"confirm": True, "signature": True, "msg": ""}
+        raise HTTPException(status_code = 404, detail = "Payment fields not found")
+    panCheck = lunh(str(payment.pan))
+    return {"confirm": _confirm, "signature": True, "code": _code}
